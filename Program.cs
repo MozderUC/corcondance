b09faee3df1a2ce3d3c9829405ddb5544311aeff
Vlad Mozder <rekeloz@gmail.com>
2017-11-09 18:31:07 +0300

    add 3: 1-Parse -for parsing sourse file, 2 - Corcondance - base class whith all main methods, 3-WordData - class that store tipical word data like word value and line where you can find this word

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace concordance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
